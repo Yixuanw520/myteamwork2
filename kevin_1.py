4c4dc498bbc9c848e6bd618646cbe890b4b66984
smarty-kev <kevinliuchen7@gmail.com>
2021-08-18 12:44:50 -0400

    modified : kevin_1.py | added kevin_2.py

--- kevin_1.py
+++ kevin_1.py
@@ -4,4 +4,14 @@ Day 1's work
 
 import random
 
-print(f"Your dice roll : {random.randint(0, 6)}")
+num = random.randint(0, 6)
+
+print(f"Your dice roll : {num}")
+
+
+# modification from day 2
+
+if num >= 4:
+    print("You won!")
+else:
+    print("Sad, try again next time.")
---
+++ kevin_2.py
@@ -0,0 +1,11 @@
+"""
+this is my work on day 2
+"""
+
+import keyboard
+import datetime
+
+print("Hello World")
+
+
+keyboard.write(f"{datetime.datetime.now()}")
